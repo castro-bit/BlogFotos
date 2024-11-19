572804be5f0c103036da6795420bfea0fd262baa
Gabriel Castro <66182560+castro-bit@users.noreply.github.com>
2024-11-19 18:25:10 -0300

    Update server.js
    Configurando servidor

--- server.js
+++ server.js
@@ -1,10 +1,50 @@
 import express from "express";
+const posts = [
+    {
+        id:1,
+        descricao: "Foto Teste",
+        imagem: "https://placecats.com/millie/300/150",
+    },
+    {
+        id:2,
+        descricao: "Paisagem deslumbrante",
+        imagem: "https://placecats.com/millie/300/150",
+       
+    },
+    {
+        id:3,
+        descricao: "Meu novo pet!",
+        imagem: "https://placecats.com/millie/300/150",
+        
+    },
+    {
+        id:4,
+        descricao: "Receita deliciosa de bolo de chocolate",
+        imagem: "https://placecats.com/millie/300/150",
+        
+    },
+    {
+        id:5,
+        descricao: "Viagem inesquecível para a Europa",
+        imagem: "https://placecats.com/millie/300/150",
+        
+    },
+];
+
 
 const app = express();
+app.use(express.json());
 app.listen(3000, () => {
     console.log("Servidor rodando na porta 3000");
 });
 
-app.get("/api", (req, res) => {
-    res.status(200).send("API rodando");
+function buscarPostporID(id){
+    return posts.findIndex((post) =>{
+        return post.id === Number(id);
+    })
+}
+
+app.get("/posts/:id", (req, res) => {
+    const index = buscarPostporID(req.params.id);
+    res.status(200).json(posts[index]);
 });
