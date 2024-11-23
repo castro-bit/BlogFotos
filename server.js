9c72bc6162b6d5480476ba449d93e946e85b9723
Gabriel Castro <66182560+castro-bit@users.noreply.github.com>
2024-11-23 01:27:44 -0300

    Integrar backend com o Frontend e API do Gemini
    Fazer a lógica de armazenamento de imagens no servidor;
    Criar a rota PUT para atualização de post;
    Integrar com o Frontend;
    Integrar com a API do Gemini;
    Fazer o deploy do back-end na Google Cloud.

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 import express from "express";
-import routes from "./routes/postsRoutes.js";
+import routes from "./src/routes/postsRoutes.js";
+
 
 const app = express();
+app.use(express.static ("uploads"));
+
 routes(app);
 // Inicia o servidor na porta 3000
 app.listen(3000, () => {
     console.log("Servidor rodando na porta 3000");
 });
+
+
+
+
--- src/models/postsModel.js
+++ src/models/postsModel.js
@@ -1,21 +1,28 @@
-import conectarAoBanco from "../config/dbConfig.js";
-
-// Conecta ao banco de dados usando a string de conexão do ambiente
+import 'dotenv/config';
+import { ObjectId } from "mongodb";
+import conectarAoBanco from "../config/dbConfig.js"
+// Conecta ao banco de dados utilizando a string de conexão fornecida como variável de ambiente
 const conexao = await conectarAoBanco(process.env.STRING_CONEXAO);
 
-
-// Função assíncrona para obter todos os posts do banco de dados
+// Função assíncrona para buscar todos os posts do banco de dados
 export async function getTodosPosts() {
-    // Seleciona o banco de dados 'blog-fotos'
+    // Seleciona o banco de dados "imersao-instabytes"
     const db = conexao.db("blog-fotos");
-    // Seleciona a coleção 'posts'
+    // Seleciona a coleção "posts" dentro do banco de dados
     const colecao = db.collection("posts");
-    // Busca todos os documentos da coleção e retorna como um array
+    // Retorna um array com todos os documentos da coleção
     return colecao.find().toArray();
 }
 
 export async function criarPost(novoPost) {
     const db = conexao.db("blog-fotos");
     const colecao = db.collection("posts");
-    return colecao.insertOne(novoPost)
+    return colecao.insertOne(novoPost);
+}
+
+export async function atualizarPost(id, novoPost) {
+    const db = conexao.db("blog-fotos");
+    const colecao = db.collection("posts");
+    const objID = ObjectId.createFromHexString(id);
+    return colecao.updateOne({_id: new ObjectId(objID)}, {$set:novoPost});
 }
